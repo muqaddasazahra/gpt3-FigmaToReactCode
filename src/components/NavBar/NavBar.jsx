f808937961354c90e6b99bf734e170545c0a749e
muqaddasazahra <muqaddasazahra@gmail.com>
2024-04-19 13:53:05 +0500

    Navbar made responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,10 @@
-import logo from "../../assets/logo.png"
-import "./NavBar.css"
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./NavBar.css";
 
 export default function NavBar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="navbar">
       <div className="nav">
@@ -29,6 +32,48 @@ export default function NavBar() {
       <div className="navbuttons">
         <button id="signIn">Sign In</button>
         <button>Sign Up</button>
+      </div>
+
+      <div className="navbarmenu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu &&
+        <div className="navmenu scale-up-center">
+          <div className="navmenulinks">
+            <p>
+              <a href="">Home</a>
+            </p>
+            <p>
+              <a href="">What is GPT3?</a>
+            </p>
+            <p>
+              <a href="">Open AI</a>
+            </p>
+            <p>
+              <a href="">case Studies</a>
+            </p>
+            <p>
+              <a href="">Library</a>
+            </p>
+          </div>
+
+          <div className="navmenubuttons">
+            <button id="signIn">Sign In</button>
+            <button>Sign Up</button>
+          </div>
+        </div>
+        }
       </div>
     </div>
   );
